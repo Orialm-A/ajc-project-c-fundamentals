ad9ec7ea63b55be6cf07f6a089acb33c0f2fafb2
Orialm-A <108948392+Orialm-A@users.noreply.github.com>
2026-08-06 16:54:12 +0200

    [ fix ] Create file config.h

---
+++ include/config.h
@@ -0,0 +1,23 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**
+ *
+ * SPDX-License-Identifier: MIT
+ * Copyright © {{pom:current_year}}
+ *
+ */
+
+/* Public API: visible to other modules */
+
+// --- includes --------------------------------------
+
+// --- typedefs and structures (scope: public) -------
+
+// --- external declarations -------------------------
+
+// --- defines and macros (scope: public) ------------
+
+// --- function prototypes (scope: public) -----------
+
+#endif /* CONFIG_H */
